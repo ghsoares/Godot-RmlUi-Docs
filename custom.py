cb3a47b4b9c206d9122b0c16e6820a40c97397df
Gabriel <ghsoares99795@gmail.com>
2026-01-22 17:00:24 -0300

    More documentation changes

--- custom.py
+++ custom.py
@@ -13,11 +13,19 @@ def configure_pages(gen):
 	})
 
 	gen.add_documentation_page({
-		"name": "Godot RmlUi",
+		"name": "Getting Started",
 		"href": "$BASE_URL/tutorial/getting_started.html",
 		"location": "/",
 		"filename": gen.dist_path("tutorial/getting_started.html"),
 		"content": gen.markup_bbcode(gen.get_file_str(gen.dst_path('pages/getting_started.bbcode')))
+	})
+
+	gen.add_documentation_page({
+		"name": "Building",
+		"href": "$BASE_URL/tutorial/building.html",
+		"location": "/",
+		"filename": gen.dist_path("tutorial/building.html"),
+		"content": gen.markup_bbcode(gen.get_file_str(gen.dst_path('pages/building.bbcode')))
 	})
 
 def configure_sidebar(gen):
@@ -26,12 +34,15 @@ def configure_sidebar(gen):
 		"items": [{
 			"href": "$BASE_URL/tutorial/getting_started.html",
 			"name": "Getting Started"
+		}, {
+			"href": "$BASE_URL/tutorial/building.html",
+			"name": "Building"
 		}]
 	})
 
 def generation_finished(gen):
 	gen.copy_folder(
 		gen.dst_path('images'),
-		gen.dist_path('images')
+		gen.dist_path('resources/images')
 	)
 
